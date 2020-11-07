5be702020398459b24a04f3eebfa4367527798f9
Rahzun <rajantamang70@gmail.com>
2020-11-07 21:05:26 +0900

    Compile index.ts to compile to output index.js

--- index.js
+++ index.js
@@ -1,3 +1,48 @@
-async function hello() {
-    return 'world';
+async function asyncExample() {
+    return 'From Async Function';
 }
+///
+/// Variable Declaration
+///
+// let age: any = 23;
+let age;
+age = '23';
+/// Here `: number`, the strong explicit type is redundnant; as we are implicitly typing it
+let age2 = 23;
+let font;
+const Player = {
+    first: 'Rahzun',
+    last: 'Tamang'
+};
+const Player2 = {
+    first: 'Leo',
+    last: 'Messi',
+    fast: true
+};
+///
+/// Function
+///
+function pow(x, y) {
+    return Math.pow(x, y).toString();
+}
+pow(2, 8);
+///
+/// Arrays
+///
+const arr = [];
+arr.push(1);
+const arr2 = [];
+arr2.push(false);
+arr2.push(54);
+arr2.push('23');
+///
+/// Generics
+///
+class Observable {
+    constructor(value) {
+        this.value = value;
+    }
+}
+let x;
+let y;
+export {};
